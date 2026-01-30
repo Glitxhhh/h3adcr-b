935ddab27ab0c8f643925c137eed301f41078a0d
Deadboys Corner <48094316+Deadboy666@users.noreply.github.com>
2026-01-29 19:56:51 -0600

    Update headcrab.sh

--- headcrab.sh
+++ headcrab.sh
@@ -129,11 +129,11 @@ set -eu
             echo "Steamos Detected"
             echo "Headcrab Bootstrapping SLSsteam.."
             createsteamcfg
-           export_sls wheresteam -clearbeta -textmode -exitsteam &> /dev/null
+           export_sls wheresteam -clearbeta -exitsteam &> /dev/null
         else
             echo "Headcrab Bootstrapping SLSsteam.."
             createsteamcfg
-            export_sls wheresteam -clearbeta -textmode -exitsteam &> /dev/null
+            export_sls wheresteam -clearbeta -exitsteam &> /dev/null
         fi
             echo "Headcrab Injected SLSsteam."
             }
