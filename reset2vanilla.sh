9c3e4966c78a5c39e6d20ac570525e735b2298aa
_drazy <48094316+Deadboy666@users.noreply.github.com>
2026-03-16 04:23:51 -0500

    Update reset2vanilla.sh

--- reset2vanilla.sh
+++ reset2vanilla.sh
@@ -8,8 +8,52 @@ FlatpakSLSsteamConfigDir=$HOME/.var/app/com.valvesoftware.Steam/.config/SLSsteam
 SLSsteamInstallDir=$HOME/.local/share/SLSsteam
 SLSsteamConfigDir=$HOME/.config/SLSsteam
 Headcrab_Downgrader_Path=$HOME/.headcrab
+TRASHITE_DIR=/usr/local/bin
 
+  read_os_release(){
+        local f
+        OS_ID=""
+        OS_ID_LIKE=""
+        for f in /etc/os-release /usr/lib/os-release; do
+            [ -r "$f" ] || continue
+            . "$f"
+            break
+        done
+        OS_ID=${ID:-}
+        OS_ID_LIKE=${ID_LIKE:-}
+    }
+    
+  bazzitecheck(){
+        read_os_release
+        [ "$OS_ID" = "bazzite" ]
+        }
+        
+  checkfortrashitestupidity(){
+    cd $TRASHITE_DIR/
+    if [ -f "steam.save" ]; then
+      echo "Purging The Deprecated Methods"
+      sudo rm {steam.save,steam}
+    elif [ -f "steam" ]; then
+      echo "Purging The Deprecated Methods"
+      sudo rm steam
+    else
+      echo "Bazzite Install Unmodified"
+    fi
+      echo "" &> /dev/null
+      }
+  
+  fixtrashite(){
+    if bazzitecheck ; then
+      checkfortrashitestupidity
+      ujust update
+    else
+        echo "" &> /dev/null
+    fi
+      echo "" &> /dev/null
+      }
 
+  
+        
   wheresteam(){
         if [ -d "$FlatpakSteamInstallDir" ]; then
                 flatpak run com.valvesoftware.Steam "$@"
@@ -26,16 +70,34 @@ Headcrab_Downgrader_Path=$HOME/.headcrab
         else
                 cd $SteamInstallDir/
             fi
-                rm steam.cfg
-                rm steam.sh
+                revertsteam 
             }
+            
+    revertsteam(){
+      if [ -f steam.cfg ]; then
+        rm steam.cfg
+      else
+        echo "steam.cfg does not exist"
+      fi
+        rm steam.sh
+        purgemillennishit
+        }
+        
+    purgemillennishit(){
+    echo "Searching For Millennium"
+      if [ -f "ubuntu12_32/libXtst.so.6" ]; then
+        echo "Unlinking Millennium"
+        rm "ubuntu12_32/libXtst.so.6"
+      else
+        echo "Millennium Not Found"
+      fi
+        echo "" &> /dev/null
+        }
 
    PurgeSLSsteam(){
         if [ -d "$FlatpakSLSsteamConfigDir" ]; then
-               #rm -rf "$FlatpakSLSsteamConfigDir"
                rm -rf "$FlatpakSLSsteamInstallDir"
         else
-              #rm -rf "$SLSsteamConfigDir"
               rm -rf "$SLSsteamInstallDir"
             fi
             }
@@ -45,12 +107,9 @@ Headcrab_Downgrader_Path=$HOME/.headcrab
     resetlaunch
     PurgeSLSsteam
     echo "Headcrab Despawned Out Of The Enviroment Reloading Save.."
-    wheresteam
+    wheresteam -exitsteam
+    fixtrashite
     }
 
 echo "Headcrab Uninstaller"
 PurgeHeadcrab
-
-
-
-
