ba51aabf0cfcda9e824be0d40bdde44d7bd8804b
_drazy <48094316+Deadboy666@users.noreply.github.com>
2026-05-12 18:32:00 -0500

    Create InstallCloudRedirect.sh

---
+++ InstallCloudRedirect.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eu
+    
+    #Paths
+    SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+    CloudRedirectApp="https://github.com/Deadboy666/h3adcr-b-modul3s/raw/refs/heads/cr-test/cloudredirect.flatpak"
+
+    install_CR(){
+    cd $SCRIPT_DIR/
+    wget -O cloudredirect.flatpak "$CloudRedirectApp"
+    echo "Installing Cloud Redirect App"
+    flatpak install --user cloudredirect.flatpak --assumeyes
+    echo "App Installed Open It To Configure Your Storage Provider"
+    }
+    install_CR
